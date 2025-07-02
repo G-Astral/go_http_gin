10ffc6d99adc87b4cb0e03e66acacdc4e7f356de
G-Astral <super.anakonda2001@yandex.ru>
2025-07-02 22:39:07 +0300

    Logger done

--- main.go
+++ main.go
@@ -35,25 +35,32 @@ func main() {
 	}
 	log.SetOutput(logFile)
 
-	logAction := func(msg string) {
-		log.Println(msg)
+	logAction := func(lvl, msg string) {
+		log.Printf("[%s] %s", lvl, msg)
 	}
 
 	r := gin.Default()
 
 	r.POST("/user", func(c *gin.Context) {
-		c.BindJSON(&user)
+		err = c.BindJSON(&user)
+		if err != nil {
+			c.JSON(400, gin.H{"error": "Неверный JSON"})
+			logAction("WARN", "Неверный JSON в теле запроса")
+			return
+		}
+
 		query = "INSERT INTO users (name, age) VALUES ($1, $2) RETURNING id"
 		err = db.QueryRow(query, user.Name, user.Age).Scan(&user.Id)
 		if err != nil {
-			c.JSON(400, gin.H{"error": "Bad request"})
+			c.JSON(500, gin.H{"error": "Bad request"})
+			logAction("ERROR", fmt.Sprintf("Ошибка при INSERT в БД: %v", err))
 			return
 		}
 
 		c.JSON(200, gin.H{
 			"message": fmt.Sprintf("Имя: %s. Возраст: %d. ID: %d", user.Name, user.Age, user.Id),
 		})
-		logAction(fmt.Sprintf("Создан пользователь: %s, %d лет (id=%d)", user.Name, user.Age, user.Id))
+		logAction("INFO", fmt.Sprintf("Создан пользователь: %s, %d лет (id=%d)", user.Name, user.Age, user.Id))
 	})
 
 	r.GET("/users", func(c *gin.Context) {
@@ -78,7 +85,7 @@ func main() {
 		}
 
 		c.IndentedJSON(200, users)
-		logAction("Запрошены все пользователи в базе данных")
+		logAction("INFO", "Запрошены все пользователи в базе данных")
 	})
 
 	r.DELETE("/user/:id", func(c *gin.Context) {
@@ -87,86 +94,96 @@ func main() {
 		id, err := strconv.Atoi(idStr)
 		if err != nil {
 			c.JSON(400, gin.H{"error": "Bad request"})
+			logAction("WARN", "Неверный ID (не число)")
 			return
 		}
 
 		res, err := db.Exec(query, id)
 		if err != nil {
 			c.JSON(500, gin.H{"error": "Internal server error"})
+			logAction("ERROR", "Ошибка при удалении строки из БД")
 			return
 		}
-
+		
 		rowsAffected, err := res.RowsAffected()
 		if err != nil {
 			c.JSON(500, gin.H{"error": "Internal server error"})
+			logAction("ERROR", fmt.Sprintf("Ошибка при получении rowsAffected:%v", err))
 			return
 		}
 
 		if rowsAffected > 0 {
 			c.JSON(200, gin.H{"message": fmt.Sprintf("Пользователь %d удален из базы данных", id)})
-			logAction(fmt.Sprintf("Удален пользователь с id: %d", id))
+			logAction("INFO", fmt.Sprintf("Удален пользователь с id: %d", id))
 		} else {
 			c.JSON(404, gin.H{"error": "Пользователь не найден"})
+			logAction("WARN", fmt.Sprintf("Пользователь с ID:%d не найден", id))
 		}
 	})
 
 	r.GET("/user/:id", func(c *gin.Context) {
-		var user User
 		query = "SELECT * FROM users WHERE id = $1"
 		idStr := c.Param("id")
 		id, err := strconv.Atoi(idStr)
 		if err != nil {
 			c.JSON(400, gin.H{"error": "Bad request"})
+			logAction("WARN", "Неверный ID (не число)")
 			return
 		}
 
 		err = db.QueryRow(query, id).Scan(&user.Id, &user.Name, &user.Age)
 		if err == sql.ErrNoRows {
-			c.JSON(404, gin.H{"error": "Bad request"})
+			c.JSON(404, gin.H{"error": "Пользователь не найден"})
+			logAction("WARN", fmt.Sprintf("Пользователь с id: %d не найден", id))
 			return
 		} else if err != nil {
 			c.JSON(500, gin.H{"error": "Internal server error"})
+			logAction("ERROR", fmt.Sprintf("Ошибка сервера: %v", err))
 			return
 		}
 
 		c.JSON(200, user)
-		logAction(fmt.Sprintf("Запрошен пользователь с id: %d", id))
+		logAction("INFO", fmt.Sprintf("Запрошен пользователь с id: %d", id))
 	})
 
 	r.PUT("/user/:id", func(c *gin.Context) {
-		var user User
 		query = "UPDATE users SET name = $1, age = $2 WHERE id = $3"
 		idStr := c.Param("id")
 		id, err := strconv.Atoi(idStr)
 		if err != nil {
 			c.JSON(400, gin.H{"error": "Bad request"})
+			logAction("WARN", "Неверный ID (не число)")
 			return
 		}
 
 		if err := c.BindJSON(&user); err != nil {
 			c.JSON(400, gin.H{"error": "Bad request"})
+			logAction("WARN", "Неверный JSON при обновлении пользователя")
 			return
 		}
 
 		res, err := db.Exec(query, user.Name, user.Age, id)
 		if err != nil {
 			c.JSON(500, gin.H{"error": "Internal server error"})
+			logAction("ERROR", fmt.Sprintf("Ошибка в запросе: %v", err))
 			return
 		}
 
 		rowsAffected, err := res.RowsAffected()
 		if err != nil {
 			c.JSON(500, gin.H{"error": "Internal server error"})
+			logAction("ERROR", fmt.Sprintf("Ошибка при обновлении строки в базе данных: %v", err))
 			return
 		}
 
 		if rowsAffected > 0 {
 			user.Id = id
 			c.JSON(200, user)
-			logAction(fmt.Sprintf("Обновлен пользователь с id: %d -> имя: %s; возраст: %d", user.Id, user.Name, user.Age))
+			logAction("INFO", fmt.Sprintf("Обновлен пользователь с id: %d -> имя: %s; возраст: %d", user.Id, user.Name, user.Age))
 			return
 		} else {
 			c.JSON(404, gin.H{"error": "Пользователь не найден"})
+			logAction("WARN", fmt.Sprintf("Пользователь с id: %d не найден", id))
 			return
 		}
 	})
